1a256ed7d1febb29e348c27f71e38258f49ad720
lmh <1121376989@qq.com>
2024-01-10 14:50:47 +0800

    feat: popup.vue

--- src/popup/popup.vue
+++ src/popup/popup.vue
@@ -1,3 +1,17 @@
 <template>
-	<div>popup</div>
+	<div class="content">
+		<iframe src="https://www.izuiyou.com/" />
+	</div>
 </template>
+<style scoped lang="less">
+.content {
+	display: block;
+	width: 800px;
+	height: 700px;
+	iframe {
+		border: none;
+		width: 100%;
+		height: 100%;
+	}
+}
+</style>
